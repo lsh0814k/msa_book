16c81555a24b73d0b40e2bff8e124280c9643911
이상훈 <slee@i-magicsoft.com>
2024-01-30 17:06:25 +0900

    도서 대여 불가능 상태로 변경 inputPort 테스트 코드 추가

---
+++ src/test/java/fem/book/application/inputport/MakeUnAvailableInputPortTest.java
@@ -0,0 +1,48 @@
+package fem.book.application.inputport;
+
+import fem.book.application.outputport.BookOutputPort;
+import fem.book.domain.model.Book;
+import fem.book.domain.model.vo.BookStatus;
+import fem.book.domain.model.vo.Classfication;
+import fem.book.domain.model.vo.Location;
+import fem.book.domain.model.vo.Source;
+import fem.book.framework.web.dto.BookOutputDTO;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MakeUnAvailableInputPortTest {
+
+    @Autowired private MakeUnAvailableInputPort makeUnAvailableInputPort;
+    @Autowired private BookOutputPort bookOutputPort;
+
+    @Test
+    @DisplayName("대여 불가능 상태로 변경")
+    void unavailable() {
+        Book book = createBook();
+        bookOutputPort.save(book);
+
+        BookOutputDTO bookOutputDTO = makeUnAvailableInputPort.unavailable(book.getNo());
+
+        assertThat(bookOutputDTO.getBookStatus()).isEqualTo(BookStatus.UNAVAILABLE.toString());
+    }
+
+    private Book createBook() {
+        return Book.enterBook(
+                "노인과 바다",
+                "헤밍웨이",
+                "202401301513",
+                "어니스트 헤밍웨이가 1952년에 발표한 중편소설",
+                LocalDate.of(1954, 1, 1),
+                Source.DONATION,
+                Classfication.LITERATURE,
+                Location.PANGYO);
+    }
+
+}
