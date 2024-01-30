4ec189f42746a3a58c4d9eeb90102ee613ab2649
이상훈 <slee@i-magicsoft.com>
2024-01-30 13:14:03 +0900

    first commit

---
+++ src/main/java/fem/book/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package fem.book.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("도서 API")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
